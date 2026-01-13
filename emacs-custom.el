6e5b36abf9cb5b040751c450f8c21e54d4bfd49d
morazotti <nicolas.morazotti@gmail.com>
2026-01-13 14:46:16 -0300

    fix: correct parenthesis in emacs-custom.el
    Adjusted the closing parenthesis in emacs-custom.el to ensure proper
    syntax. This change prevents potential issues when loading the
    configuration and improves compatibility with existing Emacs setups.

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+
  '(org-latex-preview-appearance-options
    '(:foreground default :background default :scale 2.0 :html-foreground
 		 "Black" :html-background "Transparent" :html-scale
