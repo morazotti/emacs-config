2c1c344ae93d16139499510bee5979d0a362a061
morazotti <nicolas.morazotti@gmail.com>
2026-01-14 15:38:01 -0300

    feat: Add math snippets to enhance org-mode usability
    - Introduced multiple new math snippets for org-mode to streamline input
      of common LaTeX symbols and commands.
    - Updated existing snippets to employ new functionality, improving
      user experience when working with mathematical expressions.
    - Adjusted conditions to ensure snippets are only activated when
      inside LaTeX fragments for better context handling.
    - Refactored hooks in `langs-utils-config.el` to support the new
      snippets.

--- modules/langs/langs-utils-config.el
+++ modules/langs/langs-utils-config.el
@@ -3,7 +3,7 @@
   :hook ((org-mode latex-mode C-mode julia-mode python-mode) . smartparens-mode))
 
 (use-package paredit
-  :hook ((lisp-mode lisp-interaction-mode clojure-mode emacs-lisp-mode) . paredit-mode))
+  :hook ((snippet-mode lisp-mode lisp-interaction-mode clojure-mode emacs-lisp-mode) . paredit-mode))
 
 (use-package rainbow-delimiters
   :hook ((prog-mode LaTeX-mode latex-mode org-mode) . rainbow-delimiters-mode))
@@ -21,6 +21,26 @@
   :init (yas-global-mode))
 
 (use-package yasnippet-snippets)
+
+;; yasnippet auto load
+;; add symbol 'auto on condition
+
+(defun my/yas-try-expanding-auto-snippets ()
+  (when (bound-and-true-p yas/minor-mode)
+      (let ((yas-buffer-local-condition ''(require-snippet-condition . auto)))
+        (yas-expand))))
+
+(add-hook 'post-self-insert-hook #'my/yas-try-expanding-auto-snippets)
+
+(defun my/yas-math-modify-accent (text)
+  (let ((argument t))
+    (condition-case nil
+	(progn
+          (backward-sexp)
+          (kill-sexp)
+          (delete-char 1))
+      (error (setq argument 'nil)))
+    (insert "\\" text "{" (if argument (current-kill 0) "") "}")))
 
 ;; remap -mode to -ts-mode
 (add-to-list 'major-mode-remap-alist '(clojure-mode . clojure-ts-mode))
