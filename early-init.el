d2799b9fdd8af0a8a9d586f7574427397bf9eaf4
morazotti <nicolas.morazotti@gmail.com>
2026-01-21 20:21:57 -0300

    early-init.el: Consolidate startup inhibition variables.

--- early-init.el
+++ early-init.el
@@ -1,8 +1,8 @@
 (setq gc-cons-threshold most-positive-fixnum)
-(setq package-enable-at-startup nil)
+(setq package-enable-at-startup nil
+      inhibit-startup-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message user-login-name)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(setq inhibit-startup-screen t
-      inhibit-startup-message t
-      inhibit-startup-echo-area-message user-login-name)
