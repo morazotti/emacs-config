ccc89b5523defadc2ec9e4054932ebf314126881
morazotti <nicolas.morazotti@gmail.com>
2026-02-09 16:04:43 -0300

    Refactor resnippets config for readability
    Converted spaces to tabs and harmonized formatting for better readability.
    Also, streamlined some regex snippets for efficiency in processing.
    
    * modules/langs/langs-resnippets-config.el (math-mode): Adjust indentation.
    * modules/langs/langs-resnippets-config.el (text-mode-enus): Remove redundant blank line.
    * modules/langs/langs-resnippets-config.el (text-mode-ptbr): Combine similar replacements.
    * modules/langs/langs-resnippets-config.el (text-mode): Update pattern matches.

--- modules/langs/langs-resnippets-config.el
+++ modules/langs/langs-resnippets-config.el
@@ -1,67 +1,67 @@
 (resnippets-define
-   "math-mode"
-   '(:mode (LaTeX-mode org-mode)
-     :condition (or (texmathp) (org-inside-LaTeX-fragment-p))
-     :priority 10)
-   ("hat" '("\\hat{" (resnippets-cursor) "}") :priority 1)
-   ("bar" '("\\bar{" (resnippets-cursor) "}") :priority 1)
-   ("dot" '("\\dot{" (resnippets-cursor) "}") :priority 1)
-   ("ddot" '("\\ddot{" (resnippets-cursor) "}") :priority 20)
-   ("til" '("\\tilde{" (resnippets-cursor) "}") :priority 1)
-   ("vec" '("\\vec{" (resnippets-cursor) "}") :priority 1)
-   ("cnc" '("\\cancel{" (resnippets-cursor) "}") :priority 1)
-   ("ck" '("\\check{" (resnippets-cursor) "}") :priority 1)
-   ("\\([a-zA-Z\\]+\\)hat" '("\\hat{" 1 "}"))
-   ("\\([a-zA-Z\\]+\\)bar" '("\\bar{" 1 "}"))
-   ("\\([a-zA-Z\\]+\\)dot" '("\\dot{" 1 "}"))
-   ("\\([a-zA-Z\\]+\\)ddot" '("\\ddot{" 1 "}"))
-   ("\\([a-zA-Z\\]+\\)til" '("\\tilde{" 1 "}"))
-   ("\\([a-zA-Z\\]+\\)vec" '("\\vec{" 1 "}"))
-   ("\\([a-zA-Z\\]+\\)<-" '("\\overleftarrow{" 1 "}"))
-   ("\\([a-zA-Z\\]+\\)cnc" '("\\cancel{" 1 "}"))
-   ("\\([a-zA-Z\\]+\\)ck" '("\\check{" 1 "}"))
-   ("\\([a-zA-Z\\]+\\)dag" '(1 "^{\\dag}"))
-   ("\\([a-zA-Z{\\]+\\)hh" '(1 "\\hbar"))
-   ("\\([0-9a-zA-Z(){\\]+\\)ll" '(1 "\\ell"))
-   ("\\([a-zA-Z0-9 \\]+\\)conj" '(1 "^{*}"))
-   ("\\([a-zA-Z\\]+\\)trans" '(1 "^{T}"))
-   ("\\([a-zA-Z\\]+\\)comp" '(1 "^{\\perp}"))
-   ("\\([a-zA-Z\\]+\\)perp" '(1 "^{\\perp}"))
-   ("\\([a-zA-Z\\]+\\)inv" '(1 "^{-1}"))
-   ((rx (or ",." ".,")) "\\to")
+ "math-mode"
+ '(:mode (LaTeX-mode org-mode)
+	 :condition (or (texmathp) (org-inside-LaTeX-fragment-p))
+	 :priority 10)
+ ("hat" '("\\hat{" (resnippets-cursor) "}") :priority 1)
+ ("bar" '("\\bar{" (resnippets-cursor) "}") :priority 1)
+ ("dot" '("\\dot{" (resnippets-cursor) "}") :priority 1)
+ ("ddot" '("\\ddot{" (resnippets-cursor) "}") :priority 20)
+ ("til" '("\\tilde{" (resnippets-cursor) "}") :priority 1)
+ ("vec" '("\\vec{" (resnippets-cursor) "}") :priority 1)
+ ("cnc" '("\\cancel{" (resnippets-cursor) "}") :priority 1)
+ ("ck" '("\\check{" (resnippets-cursor) "}") :priority 1)
+ ("\\([a-zA-Z\\]+\\)hat" '("\\hat{" 1 "}"))
+ ("\\([a-zA-Z\\]+\\)bar" '("\\bar{" 1 "}"))
+ ("\\([a-zA-Z\\]+\\)dot" '("\\dot{" 1 "}"))
+ ("\\([a-zA-Z\\]+\\)ddot" '("\\ddot{" 1 "}"))
+ ("\\([a-zA-Z\\]+\\)til" '("\\tilde{" 1 "}"))
+ ("\\([a-zA-Z\\]+\\)vec" '("\\vec{" 1 "}"))
+ ("\\([a-zA-Z\\]+\\)<-" '("\\overleftarrow{" 1 "}"))
+ ("\\([a-zA-Z\\]+\\)cnc" '("\\cancel{" 1 "}"))
+ ("\\([a-zA-Z\\]+\\)ck" '("\\check{" 1 "}"))
+ ("\\([a-zA-Z\\]+\\)dag" '(1 "^{\\dag}"))
+ ("\\([a-zA-Z{\\]+\\)hh" '(1 "\\hbar"))
+ ("\\([0-9a-zA-Z(){\\]+\\)ll" '(1 "\\ell"))
+ ("\\([a-zA-Z0-9 \\]+\\)conj" '(1 "^{*}"))
+ ("\\([a-zA-Z\\]+\\)trans" '(1 "^{T}"))
+ ("\\([a-zA-Z\\]+\\)comp" '(1 "^{\\perp}"))
+ ("\\([a-zA-Z\\]+\\)perp" '(1 "^{\\perp}"))
+ ("\\([a-zA-Z\\]+\\)inv" '(1 "^{-1}"))
+ ((rx (or ",." ".,")) "\\to")
 
-   ("\\(.[a-zA-Z0-9\\\\{}]+\\)\\(_{[^]]+}\\)!"
-    '(1 2 "\\!^{" (resnippets-cursor) "}"))
-   ("\\(.[a-zA-Z0-9\\\\{}]+\\)\\(\\^{[^]]+}\\)!"
-    '(1 2 "\\!_{" (resnippets-cursor) "}"))
+ ("\\(.[a-zA-Z0-9\\\\{}]+\\)\\(_{[^]]+}\\)!"
+  '(1 2 "\\!^{" (resnippets-cursor) "}"))
+ ("\\(.[a-zA-Z0-9\\\\{}]+\\)\\(\\^{[^]]+}\\)!"
+  '(1 2 "\\!_{" (resnippets-cursor) "}"))
 
-   ("\\(\\mqty.\\)\\([^])}]*\\);" '(1 2 "\\\\ " (resnippets-cursor)))
-   ("lr\\([({[]\\)" '("\\left"
-			 (if (string= "{" (resnippets-group 1))
-			     "\\{" (resnippets-group 1))
-			 " " (resnippets-cursor) " \\right"
-		       (when (string= "{" (resnippets-group 1)) "\\")))
+ ("\\(\\mqty.\\)\\([^])}]*\\);" '(1 2 "\\\\ " (resnippets-cursor)))
+ ("lr\\([({[]\\)" '("\\left"
+		    (if (string= "{" (resnippets-group 1))
+			"\\{" (resnippets-group 1))
+		    " " (resnippets-cursor) " \\right"
+		    (when (string= "{" (resnippets-group 1)) "\\")))
 
-   ("lr<" '("\\left\\langle " (resnippets-cursor) " \\right\\rangle"))
+ ("lr<" '("\\left\\langle " (resnippets-cursor) " \\right\\rangle"))
 
-   ("^," "&")
-   ("\\ket{bra" '("\ketbra{" (resnippets-cursor) "}{"))
-   ("<\\([a-zA-Z0-9_^{}\\\\]+\\)|" '("\\bra{" 1 "}"))
-   ("<\\([a-zA-Z0-9_^{}\\\\]+\\) ?|\\([a-zA-Z0-9_^{}\\\\]+\\) ?>"
-    '("\\braket{" 1 "}{" 2 "}"))
+ ("^," "&")
+ ("\\ket{bra" '("\ketbra{" (resnippets-cursor) "}{"))
+ ("<\\([a-zA-Z0-9_^{}\\\\]+\\)|" '("\\bra{" 1 "}"))
+ ("<\\([a-zA-Z0-9_^{}\\\\]+\\) ?|\\([a-zA-Z0-9_^{}\\\\]+\\) ?>"
+  '("\\braket{" 1 "}{" 2 "}"))
 
-   ("\\([\(]?\\)\\([ ]?\\)//" '(1 "\\frac{" (resnippets-cursor) "}{}"))
-   ("\\([a-zA-Z0-9{}_\\^\\\\]+\\)/" '("\\frac{" 1 "}{" (resnippets-cursor) "}"))
-   ("\\([a-zA-Z}]\\)\\([0-9]\\)" '(1 "_" 2))
-   ("_\\([0-9][0-9]\\)" '("_{" 1 (resnippets-cursor) "}"))
+ ("\\([\(]?\\)\\([ ]?\\)//" '(1 "\\frac{" (resnippets-cursor) "}{}"))
+ ("\\([a-zA-Z0-9{}_\\^\\\\]+\\)/" '("\\frac{" 1 "}{" (resnippets-cursor) "}"))
+ ("\\([a-zA-Z}]\\)\\([0-9]\\)" '(1 "_" 2))
+ ("_\\([0-9][0-9]\\)" '("_{" 1 (resnippets-cursor) "}"))
 
-   ;; fun call
-   ;; any elisp function ~fun~ with a numeric argument
-   ;; can be called by ;fun=argument;
-   (";\\([^=]+\\)=\\([\-0-9.]+\\);"
-    '((number-to-string
-       (funcall (intern (resnippets-group 1))
-                (string-to-number (resnippets-group 2)))))))
+ ;; fun call
+ ;; any elisp function ~fun~ with a numeric argument
+ ;; can be called by ;fun=argument;
+ (";\\([^=]+\\)=\\([\-0-9.]+\\);"
+  '((number-to-string
+     (funcall (intern (resnippets-group 1))
+              (string-to-number (resnippets-group 2)))))))
 
 (resnippets-define
  "text-mode-enus"
@@ -75,8 +75,7 @@
 	 :match-case t
 	 :priority 20)
  ("freq\\(s?\\) " '("frequenc" (if (string= "s" (resnippets-group 1)) "ies " "y ")))
- ("sn " "tion ")
- )
+ ("sn " "tion "))
 
 (resnippets-define
  "text-mode-ptbr"
@@ -104,18 +103,15 @@
  ("rapido " "rápido ")
  ("ruido " "ruído ")
  ("port\\(i?\\)f\\([oó]\\)lio " "portfólio ")
- ("voce " "você ")
- ("vc " "você ")
-
- )
+ ((rx (or "voce " "vc ")) "você "))
 
 (resnippets-define
  "text-mode"
  '(:mode text-mode
 	 :condition (not (or (texmathp) (org-inside-LaTeX-fragment-p)))
 	 :match-case t)
- ((rx (or "mv" "vm")) '("\\(" (resnippets-cursor) "\\)") :word-boundary t)
- ((rx (or "ts" "st")) '("\\[" (resnippets-cursor) "\\]") :word-boundary t)
+ ("mk" '("\\(" (resnippets-cursor) "\\)") :word-boundary t)
+ ("dm" '("\\[" (resnippets-cursor) "\\]") :word-boundary t)
  ("qt\\(s?\\) " '("qubit" 1 " "))
  ("qi " "qubit ")
  ("qii " "qubits ")
