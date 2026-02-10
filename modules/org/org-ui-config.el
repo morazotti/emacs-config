bc562bbde2527856e06bf68bd2edecb43b56bb2c
morazotti <nicolas.morazotti@gmail.com>
2026-02-10 14:31:57 -0300

    Revert "Update org and LaTeX configurations"
    This reverts commit 6ee6aba2c0b6e346db6e550fa5c7cafd55861ece.

--- modules/org/org-ui-config.el
+++ modules/org/org-ui-config.el
@@ -148,6 +148,35 @@
       (1 font-lock-keyword-face)
       (2 font-lock-constant-face))))
 
+;; Faces
+(add-hook 'org-mode-hook '(lambda ()
+			    (TeX-fold-mode)
+			    (set-face-attribute 'TeX-fold-folded-face nil :foreground nil :inherit 'shadow)
+			    (TeX-fold-buffer)))
+
+;; Custom folded display for labels and refs
+
+(defun my/TeX-fold-ref (text)
+  (let* ((m (string-match "^\\([^:]+:\\)\\(.*\\)" text))
+         (cat (or (match-string 1 text) ""))
+         (ref (or (match-string 2 text) text)))
+    (when (> (length ref) 13)
+        (setq ref (concat (substring ref 0 6) "..." (substring ref -6))))
+    (concat "[" (propertize cat 'face 'shadow) ref "]")))
+
+(defun my/TeX-fold-label (&rest texts)
+  (cl-loop for text in texts
+           for m = (string-match "^\\([^:]+:\\)\\(.*\\)" text)
+           for cat = (or (match-string 1 text) "")
+           for ref = (or (match-string 2 text) text)
+           collect (concat "[" (propertize cat 'face 'shadow) ref "]") into labels
+           finally return (mapconcat #'identity labels ",")))
+
+(setq-default TeX-fold-macro-spec-list
+              '((my/TeX-fold-label ("cite"))
+                (my/TeX-fold-label ("label"))
+                (my/TeX-fold-ref ("ref" "pageref" "eqref" "footref"))))
+
 ;; svg-tags: create buttons with svg
 ;; (use-package svg-lib)
 ;; (use-package svg-tag
