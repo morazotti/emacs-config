2f3c061d0b1a07e925809661efad66b210508488
morazotti <nicolas.morazotti@gmail.com>
2026-02-11 09:46:34 -0300

    modules/wip-functions.el: removing legacy code and macro experimenting

--- modules/wip-functions.el
+++ modules/wip-functions.el
@@ -43,55 +43,7 @@
 	:link link
 	:description description)))))
     
-(defvar my/latex-math-macros
-  '(("hat" . "\\hat")
-    ("bar" . "\\bar")
-    ("dot" . "\\dot")
-    ("vec" . "\\vec")
-    ("til" . "\\tilde"))
-  "Lista de sufixos e macros LaTeX para expansão automática.")
-
-(defun my/latex-expand-sexp-macro ()
-  "Expande a sexp anterior usando `my/latex-math-macros`."
-  (interactive)
-  (when (org-inside-LaTeX-fragment-p)
-    (let* ((bounds (bounds-of-thing-at-point 'sexp))
-         (sexp   (and bounds
-                      (buffer-substring-no-properties
-                       (car bounds) (cdr bounds)))))
-    (when sexp
-      (let ((match (seq-find (lambda (pair)
-                               (string-suffix-p (car pair) sexp))
-                             my/latex-math-macros)))
-        (when match
-          (let* ((suffix (car match))
-                 (macro  (cdr match))
-                 (base   (substring sexp 0 (- (length sexp) (length suffix))))
-                 (start  (car bounds))
-                 (end    (cdr bounds)))
-            (delete-region start end)
-            (insert macro "{" base "}")
-            (if (string-empty-p base)
-                (backward-char 1)
-              (goto-char (+ start (length macro) 2 (length base)))))))))))
-
-(defun my/latex-trigger-expand ()
-  (when (and (derived-mode-p 'org-mode)
-             (org-inside-LaTeX-fragment-p))
-    (let ((last (char-to-string last-command-event)))
-      (when (seq-some (lambda (pair)
-                        (string-suffix-p last (car pair)))
-                      my/latex-math-macros)
-        (my/latex-expand-sexp-macro)))))
-
 (add-hook 'post-self-insert-hook #'my/latex-trigger-expand)
-
-(use-package aas
-  :hook (org-mode . aas-activate-for-major-mode)
-  :config
-  (aas-set-snippets 'org-mode
-    :cond #'org-inside-LaTeX-fragment-p
-    "ht" "hat"))
 
 (provide 'org-notmuch)
 (provide 'wip-functions)
