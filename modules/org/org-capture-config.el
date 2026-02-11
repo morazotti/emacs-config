d53da7d09ac29acdd15c807123ce7fc8733d2071
morazotti <nicolas.morazotti@gmail.com>
2026-02-11 18:21:27 -0300

    modules/org/org-capture-config.el: org capture templates for religious activities

--- modules/org/org-capture-config.el
+++ modules/org/org-capture-config.el
@@ -5,7 +5,17 @@
          "* TODO %?\n")
         ("i" "Inbox" entry (file+headline "~/Documents/org/notes.org" "Inbox")
          "* TODO <%<%Y-%m-%d %H:%M:%S>> \n %?\n")
-  	("t" "Tasks" entry (file+headline "~/Documents/org/tasks.org" "Tarefas")
+        ("v" "Grupo de Virtudes" entry
+	 (file+olp "~/Documents/org/tasks.org" "Religião" "Grupo de Virtudes")
+	 "* TODO [N] Grupo de Virtudes\n SCHEDULED: %^T"
+	 :immediate-finish t)
+	("r" "Recolhimento" entry
+	 (file+olp "~/Documents/org/tasks.org" "Religião" "Recolhimento")
+	 "* TODO [N] Recolhimento\n SCHEDULED: %^T"
+	 :immediate-finish t)
+
+	("t" "Tasks" entry (file+headline "~/Documents/org/tasks.org" "Tarefas")
          "* TODO %?\n")))
+
 (global-set-key (kbd "C-c c") 'org-capture)
 (provide 'org-capture-config)
