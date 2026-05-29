e7a2043312b96b8ef2f813d30ee930dca57b1e6c
Nícolas André da Costa Morazotti <nicolas.morazotti@gmail.com>
2026-05-29 16:56:51 -0300

    early-init.el: adding user-lisp-directory to prepare for 31.1

--- early-init.el
+++ early-init.el
@@ -2,7 +2,8 @@
 (setq package-enable-at-startup nil
       inhibit-startup-screen t
       inhibit-startup-message t
-      inhibit-startup-echo-area-message user-login-name)
+      inhibit-startup-echo-area-message user-login-name
+      user-lisp-directory (file-name-concat user-emacs-directory "modules"))
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
