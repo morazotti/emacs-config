d10bb3277cb409093a6f84ad1cf9c24c66b4bb17
Nícolas André da Costa Morazotti <nicolas.morazotti@gmail.com>
2026-05-29 17:41:22 -0300

    modules/core/core-custom-functions-config.el: Simplify comments.

--- modules/core/core-custom-functions-config.el
+++ modules/core/core-custom-functions-config.el
@@ -1,13 +1,10 @@
 (defun my/define-repeat-map (name bindings)
   "Cria um repeat-map dinamicamente sem quebrar o compilador."
-  (let ((map-name (intern (format "my-%s-repeat-map" name)))
+  (let ((map-name (intern (format "my/%s-repeat-map" name)))
         (map (make-sparse-keymap)))
-    ;; 1. Popula o keymap com os atalhos
     (dolist (binding bindings)
       (define-key map (car binding) (cdr binding)))
-    ;; 2. Define a variável global do mapa se ela não existir
     (set map-name map)
-    ;; 3. Vincula cada comando ao mapa criado
     (dolist (binding bindings)
       (put (cdr binding) 'repeat-map map-name))))
 
