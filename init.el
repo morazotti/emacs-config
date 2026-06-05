f813d0a8bb8efd4b0096312640729ea9bc004861
Nícolas André da Costa Morazotti <nicolas.morazotti@gmail.com>
2026-06-05 09:11:13 -0300

    init.el: Fix typo and improve messages clarity.

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
 ;;   (advice-add hook-fn :before
 ;;               (lambda (&rest _)
 ;;                 (message "Executando %s..." hook-fn))))
+
 (defun my/profile-kill-emacs-hook ()
   "Mede o tempo de execução de cada função no kill-emacs-hook."
   (let ((hooks (remq 'my/profile-kill-emacs-hook kill-emacs-hook)))
@@ -25,12 +26,12 @@
     (setq kill-emacs-hook nil)
     (dolist (fn hooks)
       (let ((start (current-time)))
-        (message "Fechando: Executando %s..." fn)
+        (message "Quitting: Running %s..." fn)
         (condition-case err
             (funcall fn)
-          (error (message "Erro em %s: %s" fn err)))
-        (message "Fechando: %s finalizado em %.3f segundos." 
+          (error (message "Error at %s: %s" fn err)))
+        (message "Quitting: %s ended in %.3f seconds."
                  fn (float-time (time-since start)))))))
 
 ;; O depth -100 garante que nossa função seja a primeira a rodar no desligamento
-(add-hook 'kill-emacs-hook #'my/profile-kill-emacs-hook -100)
+(add-hook 'kill-emacs-hook #'my/profile-kill-emacs-hook -100)
