e910c9747b2184c8fb738f8befbf072ad3d0851c
nmorazotti <nicolas.morazotti@gmail.com>
2026-07-24 19:32:36 -0300

    modules/ai/ai-ollama-config.el: Remove inactive code and update model list

--- modules/ai/ai-ollama-config.el
+++ modules/ai/ai-ollama-config.el
@@ -1,35 +1,10 @@
 (gptel-make-ollama "Ollama"
   :host "localhost:11434"
   :stream t
-  :models '("llama3.1:8b"
-	    "phi3.5:latest"
-	    "deepseek-coder-v2:lite"))
-
-(defun my/ollama-active-p ()
-  "Return t if the Ollama service is active in systemd."
-  (let ((status (string-trim (shell-command-to-string "systemctl is-active ollama"))))
-    (string= status "active")))
-
-(defun ollama-status-toggle ()
-  "Toggle the Ollama service based on the predicate my/ollama-active-p."
-  (interactive)
-  (if (my/ollama-active-p)
-      (progn
-        (shell-command "sudo systemctl stop ollama")
-        (message "Started ollama."))
-    (progn
-      (shell-command "sudo systemctl start ollama")
-      (message "Stopped ollama."))))
-
-(advice-add 'gptel-send :before
-            (lambda (&rest _)
-              (let ((backend (or gptel-backend (default-value 'gptel-backend))))
-                (when (and (cl-typep backend 'gptel-ollama)
-                           (not (my/ollama-active-p)))
-                  (when (y-or-n-p "Ollama is off. Do you want to start it now?")
-                    (ollama-status-toggle)
-                    ;; Give a small delay for the socket to open
-                    (sleep-for 1))))))
+  :models '("llama3.1"
+	    "qwen2.5:7b"
+	    "qwen2.5-coder:7b"
+	    "deepseek-r1:7b"))
 
 (setq-default gptel-model "llama3.1:8b")
 (provide 'ai-ollama-config)
